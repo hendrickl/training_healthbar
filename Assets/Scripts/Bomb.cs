e02729933baf3ec15af52a400dedc29fa1da2062
hendrickl <65900996+hendrickl@users.noreply.github.com>
2023-01-12 12:25:06 +0100

    Point 7

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -6,8 +6,20 @@ public class Bomb : MonoBehaviour
 {
     [SerializeField] private int _damage = 10;
 
-    // private void OnMouseDown()
-    // {
-    //     print("test");
-    // }
+    private Vector3 _screenPosition;
+    private Vector3 _offset;
+
+    void OnMouseDown()
+    {
+        _screenPosition = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        _offset = gameObject.transform.position -
+                Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, _screenPosition.z));
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 currentScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, _screenPosition.z);
+        Vector3 currentPosition = Camera.main.ScreenToWorldPoint(currentScreenPoint) + _offset;
+        transform.position = currentPosition;
+    }
 }
